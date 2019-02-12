1ca59385ddccf64db30ad4b00c4111b80fc73211
batflyer <alexander@batflyer.net>
2019-02-12 12:08:35 -0500

    Adding project `setup.py`

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Alexander L. Hayes
+
+from setuptools import setup
+
+setup(
+    name='PHI',
+    version='0.0.1',
+    packages=['phi'],
+    url='https://github.com/batflyer/PHI',
+    license='Not-Declared',
+    author='Alexander L. Hayes (@batflyer)',
+    author_email='alexander@batflyer.net',
+    description='Python API for experimenting with Precision Health Data'
+)
